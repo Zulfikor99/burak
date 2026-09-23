f4a2ffdd684587482882daad5cad18bb997e775f
Zulfikor Sayfiev <zulfikordev@gmail.com>
2026-09-24 00:53:57 +0900

    feat:connect MongoDb via Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URI as string, {})
+  .then((data) => {
+    console.log("MongoDb connection succeed");
+    const PORT = process.env.PORT;
+  })
+  .catch((err) => {
+    console.log("Error on connection MongoDB", err);
+  });
