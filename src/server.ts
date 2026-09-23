d48c4cabdaac4739d62b725cdd5284713cc056ea
Zulfikor Sayfiev <zulfikordev@gmail.com>
2026-09-24 01:49:20 +0900

    feat:express integration

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
+import app from "./app";
 mongoose
   .connect(process.env.MONGO_URI as string, {})
   .then((data) => {
     console.log("MongoDb connection succeed");
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running on Port:${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Error on connection MongoDB", err);
--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,28 @@
-console.log("==========TASK L==========");
+console.log("==========TASK M==========");
 
 /*
+
+Shunday function yozing, u raqamlardan tashkil topgan
+array qabul qilsin va array ichidagi har bir raqam uchun
+raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan
+object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+MASALAN: getSquareNumbers([1, 2, 3])
+return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+
+*/
+
+function getSquareNumbers(arr: number[]) {
+  return arr.map((ele: number) => ({
+    number: ele,
+    square: ele * ele,
+  }));
+}
+console.log(getSquareNumbers([1, 2, 3, 4]));
+
+/*
+console.log("==========TASK L==========");
+
+
 
 Shunday function yozing, u string qabul qilsin
 
@@ -12,6 +34,7 @@ MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc!";
 
 */
 
+/*
 function reverseSentence(str: string): string {
   let words: string[] = str.split(" ");
 
@@ -23,3 +46,5 @@ function reverseSentence(str: string): string {
 }
 
 console.log(reverseSentence("we like coding!"));
+
+*/
