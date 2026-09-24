596e4def42c9a9b5391ed0fb5fbb1427b8103417
Zulfikor Sayfiev <zulfikordev@gmail.com>
2026-09-25 02:35:30 +0900

    feat:create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1 - Enterance   **/
 const app = express();
@@ -8,12 +9,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/** 1 - Sessions   **/
+/** 2 - Sessions   **/
 
-/** 1 - Views   **/
+/** 3 - Views   **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-/** 1 - Routers   **/
+/** 4 - Routers   **/
+app.use("/", router); // Middleware Design Pattern
 
 export default app;
