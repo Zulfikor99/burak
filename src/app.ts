255f06cd561d31663aed4c6e6d0b4d3ad1831502
Zulfikor Sayfiev <zulfikordev@gmail.com>
2026-09-25 02:52:27 +0900

    fix:modify routing sytem and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1 - Enterance   **/
 const app = express();
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4 - Routers   **/
-app.use("/", router); // Middleware Design Pattern
+app.use("/admin", routerAdmin); // BSSR: EJS
+app.use("/", router); // SPA: REACT
 
 export default app;
